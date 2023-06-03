b74dc8e7ea8a9f371a56d2e3c28b4a8aa4070b71
Shoeb <shoeb@cruxcreativesolutions.com>
2023-06-03 17:19:02 +0530

    Some Chhanges

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
+import Link from 'next/link'
 import React from 'react'
-
+ 
 const Footer = () => {
   return (
 <div className="card mt-3">
@@ -9,7 +10,7 @@ const Footer = () => {
   <div className="card-body">
     <h5 className="card-title">Special title treatment</h5>
     <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <Link href='/'>Go somewhere</Link>
   </div>
 </div>
   )
